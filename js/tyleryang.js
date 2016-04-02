260b9bc02aa312578e6a73dde6072cfcfb548814
TylerYang <tyler.z.yang@gmail.com>
2016-04-02 19:06:44 +0800

    Site updated: 2016-04-02 19:06:43

---
+++ js/tyleryang.js
@@ -0,0 +1,40 @@
+var tyleryang = {
+    name: 'Tyler Yang',
+    age: 26,
+    contacts: {
+        email: 'tyler.z.yang@gmail.com',
+        skype: 'tyleryang.24'
+    },
+    educations: [
+        {
+            year: 2013,
+            institution: 'Guang Dong University of Foreign Studies',
+            degree: 'Bachelor in Computer Science'
+        }
+    ],
+    company: {
+        name: 'Netease Games',
+        title: 'Senior Frontend Developer',
+        responsibilities: [
+            'Creating solutions architecture, algorithms, and designs for solutions scale to the requirements.',
+            'Developing expertise in products',
+            'Leading a small team of frontend engineers to apply software engineering pratices'
+        ]
+    },
+    skillsets: [
+        'react-*', 'angular', 'node', 'npm', 'webpack', 'jasmine', 'java', 'maven'
+    ],
+    links: {
+        github: 'https://github.com/tyleryang',
+        stackoverflow: {
+            about: 'http://stackoverflow.com/users/3064056/tyler-z-yang',
+            recentAnswer: 'http://stackoverflow.com/questions/36241174/how-to-build-image-with-webpack-for-react/36242974#36242974'
+        }
+    },
+    words: [
+        'Simpility is the prerequisite of reliability.', 'Simple made easy.'
+    ],
+    tags: [
+        'JavaScript', 'Unix', 'Vim', 'Algorithm'
+    ]
+};
